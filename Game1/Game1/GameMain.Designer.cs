574078f7392c51a93e354f3bcc4ffbdd24295b27
Ali Saadatzadeh <67889979+AliSaadatzadeh73@users.noreply.github.com>
2021-10-17 16:13:43 +0330

    Add files via upload

---
+++ Game1/Game1/GameMain.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Game1
+{
+    partial class GameMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameMain));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnMinimized = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnExit = new Bunifu.Framework.UI.BunifuImageButton();
+            this.lblScore = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.vert = new System.Windows.Forms.Timer(this.components);
+            this.hor = new System.Windows.Forms.Timer(this.components);
+            this.watch = new System.Windows.Forms.Timer(this.components);
+            this.pnlGameOver = new System.Windows.Forms.Panel();
+            this.btnRefreshGame = new Bunifu.Framework.UI.BunifuImageButton();
+            this.lblValue = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.picslid = new System.Windows.Forms.PictureBox();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.pnlPause = new System.Windows.Forms.Panel();
+            this.bunifuCustomLabel4 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
+            this.pnlGameOver.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRefreshGame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picslid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.pnlPause.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this.pnlHeader;
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.DimGray;
+            this.pnlHeader.Controls.Add(this.btnMinimized);
+            this.pnlHeader.Controls.Add(this.btnExit);
+            this.pnlHeader.Controls.Add(this.lblScore);
+            this.pnlHeader.Controls.Add(this.bunifuCustomLabel1);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(921, 32);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // btnMinimized
+            // 
+            this.btnMinimized.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimized.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.btnMinimized.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimized.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimized.Image")));
+            this.btnMinimized.ImageActive = null;
+            this.btnMinimized.Location = new System.Drawing.Point(841, 0);
+            this.btnMinimized.Name = "btnMinimized";
+            this.btnMinimized.Size = new System.Drawing.Size(40, 32);
+            this.btnMinimized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimized.TabIndex = 3;
+            this.btnMinimized.TabStop = false;
+            this.btnMinimized.Zoom = 10;
+            this.btnMinimized.Click += new System.EventHandler(this.btnMinimaize_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageActive = null;
+            this.btnExit.Location = new System.Drawing.Point(881, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(40, 32);
+            this.btnExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnExit.TabIndex = 1;
+            this.btnExit.TabStop = false;
+            this.btnExit.Zoom = 10;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblScore.Location = new System.Drawing.Point(67, 6);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(18, 19);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "0";
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(5, 4);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(60, 21);
+            this.bunifuCustomLabel1.TabIndex = 4;
+            this.bunifuCustomLabel1.Text = "Score :";
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.pnlHeader;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // vert
+            // 
+            this.vert.Enabled = true;
+            this.vert.Tick += new System.EventHandler(this.vert_Tick);
+            // 
+            // hor
+            // 
+            this.hor.Enabled = true;
+            this.hor.Tick += new System.EventHandler(this.hor_Tick);
+            // 
+            // watch
+            // 
+            this.watch.Enabled = true;
+            this.watch.Tick += new System.EventHandler(this.watch_Tick);
+            // 
+            // pnlGameOver
+            // 
+            this.pnlGameOver.BackColor = System.Drawing.Color.MintCream;
+            this.pnlGameOver.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlGameOver.Controls.Add(this.btnRefreshGame);
+            this.pnlGameOver.Controls.Add(this.lblValue);
+            this.pnlGameOver.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.pnlGameOver.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlGameOver.Location = new System.Drawing.Point(166, 167);
+            this.pnlGameOver.Name = "pnlGameOver";
+            this.pnlGameOver.Size = new System.Drawing.Size(506, 180);
+            this.pnlGameOver.TabIndex = 2;
+            this.pnlGameOver.Visible = false;
+            // 
+            // btnRefreshGame
+            // 
+            this.btnRefreshGame.BackColor = System.Drawing.Color.LightYellow;
+            this.btnRefreshGame.Image = global::Game1.Properties.Resources.refresh_reload_icon1;
+            this.btnRefreshGame.ImageActive = null;
+            this.btnRefreshGame.Location = new System.Drawing.Point(201, 115);
+            this.btnRefreshGame.Name = "btnRefreshGame";
+            this.btnRefreshGame.Size = new System.Drawing.Size(70, 58);
+            this.btnRefreshGame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnRefreshGame.TabIndex = 2;
+            this.btnRefreshGame.TabStop = false;
+            this.btnRefreshGame.Zoom = 10;
+            this.btnRefreshGame.Click += new System.EventHandler(this.btnRefreshGame_Click);
+            // 
+            // lblValue
+            // 
+            this.lblValue.BackColor = System.Drawing.Color.LightYellow;
+            this.lblValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblValue.Location = new System.Drawing.Point(0, 0);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(502, 176);
+            this.lblValue.TabIndex = 0;
+            this.lblValue.Text = "Game Over";
+            this.lblValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(10, 35);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(55, 21);
+            this.bunifuCustomLabel2.TabIndex = 5;
+            this.bunifuCustomLabel2.Text = "level :";
+            // 
+            // bunifuCustomLabel3
+            // 
+            this.bunifuCustomLabel3.AutoSize = true;
+            this.bunifuCustomLabel3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.bunifuCustomLabel3.Location = new System.Drawing.Point(67, 38);
+            this.bunifuCustomLabel3.Name = "bunifuCustomLabel3";
+            this.bunifuCustomLabel3.Size = new System.Drawing.Size(19, 21);
+            this.bunifuCustomLabel3.TabIndex = 6;
+            this.bunifuCustomLabel3.Text = "0";
+            // 
+            // picslid
+            // 
+            this.picslid.BackColor = System.Drawing.Color.Transparent;
+            this.picslid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picslid.Image = global::Game1.Properties.Resources.takht;
+            this.picslid.Location = new System.Drawing.Point(-1, -1);
+            this.picslid.Name = "picslid";
+            this.picslid.Size = new System.Drawing.Size(146, 14);
+            this.picslid.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picslid.TabIndex = 1;
+            this.picslid.TabStop = false;
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.Transparent;
+            this.picBall.Image = global::Game1.Properties.Resources.ball;
+            this.picBall.Location = new System.Drawing.Point(127, 35);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(80, 65);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // pnlPause
+            // 
+            this.pnlPause.BackColor = System.Drawing.Color.MintCream;
+            this.pnlPause.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlPause.Controls.Add(this.bunifuCustomLabel4);
+            this.pnlPause.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.pnlPause.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlPause.Location = new System.Drawing.Point(168, 165);
+            this.pnlPause.Name = "pnlPause";
+            this.pnlPause.Size = new System.Drawing.Size(506, 180);
+            this.pnlPause.TabIndex = 7;
+            this.pnlPause.Visible = false;
+            // 
+            // bunifuCustomLabel4
+            // 
+            this.bunifuCustomLabel4.BackColor = System.Drawing.Color.LightYellow;
+            this.bunifuCustomLabel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuCustomLabel4.Location = new System.Drawing.Point(0, 0);
+            this.bunifuCustomLabel4.Name = "bunifuCustomLabel4";
+            this.bunifuCustomLabel4.Size = new System.Drawing.Size(502, 176);
+            this.bunifuCustomLabel4.TabIndex = 0;
+            this.bunifuCustomLabel4.Text = "Game Paused";
+            this.bunifuCustomLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.picslid);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 572);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(921, 14);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Lime;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.picBall);
+            this.panel2.Controls.Add(this.pnlGameOver);
+            this.panel2.Controls.Add(this.pnlPause);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(921, 540);
+            this.panel2.TabIndex = 9;
+            // 
+            // GameMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(921, 586);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.bunifuCustomLabel3);
+            this.Controls.Add(this.bunifuCustomLabel2);
+            this.Controls.Add(this.pnlHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "GameMain";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameMain_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameMain_KeyUp);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
+            this.pnlGameOver.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnRefreshGame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picslid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.pnlPause.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel pnlHeader;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private Bunifu.Framework.UI.BunifuImageButton btnExit;
+        private Bunifu.Framework.UI.BunifuImageButton btnMinimized;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.PictureBox picslid;
+        private System.Windows.Forms.Timer vert;
+        private System.Windows.Forms.Timer hor;
+        private System.Windows.Forms.Timer watch;
+        private System.Windows.Forms.Panel pnlGameOver;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblValue;
+        private Bunifu.Framework.UI.BunifuCustomLabel lblScore;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        private Bunifu.Framework.UI.BunifuImageButton btnRefreshGame;
+        private System.Windows.Forms.Panel pnlPause;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Game1/Game1/GameMain.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game1
+{
+    public partial class GameMain : Form
+    {
+        public GameMain()
+        {
+            InitializeComponent();
+        }
+        int speed = 16;
+        int vdir = +1;
+        int hdir = +1;
+        int slide_speed = 50;
+        bool gameOver = false;
+        int score = 0;
+        bool _left = false, _right = false;
+        bool pause
+        {
+            get
+            { return !pnlPause.Visible; }
+            set
+            { pnlPause.Visible = !value; }
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void vert_Tick(object sender, EventArgs e)
+        {
+            if (picBall.Top < 0)
+            {
+                vdir = +1;
+            }
+            else if (picBall.Top > (this.Height - picBall.Height - picslid.Height))
+            {
+                vdir = -1;
+
+                if (picBall.Left < picslid.Left || ((picBall.Left + picBall.Width) > (picslid.Left + picslid.Width)))
+                {
+                    vert.Enabled = hor.Enabled = false;
+                    pnlGameOver.Visible = true;
+                    gameOver = true;
+                    lblValue.BackColor = Color.DarkRed;
+                    btnRefreshGame.BackColor = Color.DarkRed;
+                    lblValue.Text = "Game Over!";
+
+                }
+                else
+                {
+                    score++;
+                    speed++;
+                }
+
+            }
+            lblScore.Text = score.ToString();
+            picBall.Top += vdir * speed;
+        }
+        private void hor_Tick(object sender, EventArgs e)
+        {
+            if (picBall.Left < 0)
+            {
+                hdir = +1;
+            }
+            else if (picBall.Left > (this.Width - picBall.Width))
+            {
+                hdir = -1;
+            }
+            picBall.Left += hdir * speed;
+        }
+        private void GameMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                _right = true;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                _left = true;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                vert.Enabled = hor.Enabled = pause = false;
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                vert.Enabled = hor.Enabled = pause = true;
+            }
+        }
+        private void watch_Tick(object sender, EventArgs e)
+        {
+            if (_right && picslid.Left < (this.Width - picslid.Width))
+            {
+                picslid.Left = picslid.Left + slide_speed;
+            }
+            else if (_left && picslid.Left > 0)
+            {
+                picslid.Left = picslid.Left - slide_speed;
+
+            }
+
+        }
+
+        private void btnRefreshGame_Click(object sender, EventArgs e)
+        {
+            vert.Enabled = hor.Enabled = true;
+            lblScore.Text = "0";
+            pnlGameOver.Visible = false;
+        }
+        private void btnMinimaize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+
+        }
+
+        private void GameMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                _right = _left = false;
+            }
+            else
+            {
+                _left = true;
+            }
+        }
+    }
+}
